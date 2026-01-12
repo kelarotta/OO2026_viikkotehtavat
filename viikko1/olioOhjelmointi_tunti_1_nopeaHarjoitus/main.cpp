03db134a13d391c0390629c4fd4e078b4a08e729
kelarotta <100769361+kelarotta@users.noreply.github.com>
2026-01-12 18:48:24 +0200

    testattiin gitignoren toiminta

---
+++ viikko1/olioOhjelmointi_tunti_1_nopeaHarjoitus/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream> // voidaan käyttää c++ juttuja
+
+using namespace std; //
+
+int main()
+{
+    int turhaLoppu;
+    int koe = 5;
+    printf("Hello world C:n tavalla, %d \n", koe);
+    cout << "Hello World!" << koe << endl;
+    scanf("%d", turhaLoppu);
+    return 0;
+}
